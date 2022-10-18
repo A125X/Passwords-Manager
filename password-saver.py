7025fa82021cdca73266ea9db8b1b8fc1a2497e1
A125X <alexander.andr.shat@gmail.com>
2022-10-18 20:46:18 +0300

    =everything is working again after a little bit of refactoring

--- password-saver.py
+++ password-saver.py
@@ -1,6 +1,13 @@
 import random
 import copy
 
+#Startup|-- User -- set_passphrase -- ask_passphrase
+#       |       |______________________________________
+#       |-- Storage -- set_database -- load_database --|get_passphrase
+#       |       |__________________
+#       |-- Session -- set_user --|start_session|
+#                                               |get_database
+
 class Key:
     def __init__(self):
         pass
@@ -52,38 +59,78 @@ class Message:
     def set_body(self, body: str) -> None:
         self.__body__ = body
 
-class User:
-    def __init__(self):
-        self.__passphrase__: str = ''
-        self.__database__:list = []
+class Storage:
+    def __init__(self, database: list=None) -> None:
+        if database is not None:
+            self.__database__ = database
+        else:
+            self.__database__:list = []
 
     #getters
     def get_database(self) -> list:
         return self.__database__
 
-    def get_passphrase(self) -> str:
-        return self.__passphrase__
-
     #setters
     def set_database(self, database: list) -> None:
         self.__database__ = database
 
-    def set_passphrase(self, passphrase: str) -> None:
-        self.__passphrase__ = passphrase
-    
     #database saver
-    def save_database(self, path: str):
+    def save_database(self, passphrase: str):
+        path: str = 'db.txt'
         with open(path, 'w') as file:
             file.writelines(data.get_title() + '\n' + data.get_body() + '\n' * 2 for data in self.get_database())
+            file.close()
+
+    #database loader
+    def load_database(self, passphrase: str):
+        return [
+            Message('test_title1', 'test_body1', is_encrypted=False),
+            Message('test_title2', 'test_body2', is_encrypted=False),
+            Message('test_title3', 'test_body3', is_encrypted=False)
+            ]
+        '''
+        path = ''
+        with open(path, 'r') as file:
+            df = file.read()
+            print(df)
+            file.close()
+        '''
+
+class User:
+    def __init__(self, passphrase=None, storage=None):
+        if passphrase is not None:
+            self.__passphrase__: str = passphrase
+        else:
+            self.__passphrase__: str = ''
+
+        if storage is not None:
+            self.__storage__: Storage = storage
+        else:
+            self.__storage__: Storage = Storage()
+
+    #getters
+    def get_passphrase(self) -> str:
+        return self.__passphrase__
+
+    def get_storage(self) -> Storage:
+        return self.__storage__
+
+    #setters
+    def set_passphrase(self, passphrase: str) -> None:
+        self.__passphrase__ = passphrase
+
+    def set_storage(self, storage: Storage) -> None:
+        self.__storage__ = storage
 
 class Startup:
     def __init__(self) -> None:
         pass
 
     def startup(self) -> None:
-        user: User = User()
-        user.set_passphrase = self.ask_passphrase()
-        user.set_database(self.recieve_database(user.get_passphrase()))
+        user: User = User(passphrase=self.ask_passphrase())
+        storage: Storage = Storage()
+        storage.set_database(storage.load_database(user.get_passphrase()))
+        user.set_storage(storage)
         session: Session = Session()
         session.set_user(user)
         session.start_session()
@@ -96,7 +143,11 @@ class Startup:
         elif passphrase_input == 'n':
             key: Key = Key()
             test_key = key.get_key()
-            print('Your passphrase is: ', test_key, '\n' + 'Please write it down before proceed!')
+            print(
+                'Your passphrase is: ', 
+                test_key, 
+                '\n' + 'Please write it down before proceed!'
+                )
             print('Type any key to proceed...')
             input()
             return test_key
@@ -104,22 +155,15 @@ class Startup:
             print('Please enter y/n')
             self.ask_passphrase()
 
-    def recieve_database(self, passphrase: str) -> list:
-        return [
-            Message('test_title1', 'test_body1', is_encrypted=False),
-            Message('test_title2', 'test_body2', is_encrypted=False),
-            Message('test_title3', 'test_body3', is_encrypted=False)
-            ]
-
 class Session:
     def __init__(self, user=User()) -> None:
-        self.user: User = user
+        self.__user__: User = user
 
     def get_user(self) -> User:
-        return self.user
+        return self.__user__
 
     def set_user(self, user: User) -> None:
-        self.user = user
+        self.__user__ = user
 
     def start_session(self) -> None:
         while True:
@@ -134,7 +178,7 @@ class Session:
             answer = input()
             #show all the messages
             if answer == 'show':
-                for item in self.user.get_database():
+                for item in self.__user__.get_storage().get_database():
                     print(item.get_title(), item.get_body())
                 print('')
             #add a new message
@@ -142,9 +186,9 @@ class Session:
                 title: str = input('Enter title of the message: \n')
                 body: str = input('Enter body of the message: \n')
                 message: Message = Message(title, body, is_encrypted=False)
-                df = self.user.get_database()
+                df = self.__user__.get_storage().get_database()
                 df.append(message)
-                self.user.set_database(df)
+                self.__user__.get_storage().set_database(df)
                 print('New message added \n')
             #edit message
             elif answer == 'edit':
@@ -160,19 +204,17 @@ class Session:
                 break
             #unexpected input
             else:
-                print('Unexpected input: ', answer, '\n' + 'Please provide expected input')
+                print(
+                    'Unexpected input: ', 
+                    answer, 
+                    '\n' + 'Please provide expected input'
+                    )
             
-            self.user.save_database('db.txt')
+            self.__user__.get_storage().save_database('db.txt')
 
 def __main__() -> int:
     startup: Startup = Startup()
     startup.startup()
-    
-    '''user: User = User()
-    user.set_database(startup.recieve_database('df'))
-    session: Session = Session()
-    session.set_user(user)
-    session.start_session()'''
     return 0
 
 if __name__ == '__main__':
